aaf3ad587930f2e17e723779f1711cc91990516a
nayana005 <nayanasuresh1005@gmail.com>
2022-08-16 19:08:28 +0530

    Added: Selecting Letter

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,14 +1,20 @@
-package com.bl.worshop1;
+package com.bridgelab.worshop1;
 
-public class TicTacToeGame {
+import java.util.Scanner;
 
+public class TicTacToeGame
+{
 	static char[] board = new char[10];
+	static char playerLetter;
+	static char computerLetter;
 	public static void main(String[] args)
 	{
 		createEmptyBoard();
+		selectLetter();
 
 	}
 
+	//UC1
 	private static void createEmptyBoard()
 	{
 		for(int index=1; index<board.length;index++)
@@ -16,6 +22,14 @@ public class TicTacToeGame {
 			board[index] = ' ';
 		}
 	}
-}
 
+	//UC2
+	private static void selectLetter()
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Choose a letter :: X or O : ");
+		playerLetter = scanner.next().toUpperCase().charAt(0);
+		computerLetter = (playerLetter == 'X') ? 'O' : 'X';
+	}
+}
 
